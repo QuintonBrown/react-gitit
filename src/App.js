8525d7f3f473e757b701a786c27bdd59d0d35c0d
Quinton H Brown <quintonbrown8101@yahoo.com>
2020-07-16 12:26:08 -0400

    fixed alt tag

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class Card extends React.Component {
     const profile = this.props;
     return (
       <div className="github-profile">
-        <img src={profile.avatar_url} atl={profile.name} />
+        <img src={profile.avatar_url} alt={profile.name} />
         <div className="info">
           <div className="name">{profile.name}</div>
           <div className="company">{profile.company}</div>
